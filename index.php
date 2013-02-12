3b7f538eee75e72b48b134f5af9333c38d71fde3
Travis Ward <travis@travsiward.com>
2013-02-12 15:53:48 -0600

    Added Blog page and callout area.

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package AgriFlex
+ * @since AgriFlex 1.0
+ */
+?>
+
+<?php get_header(); ?>
+
+  <div id="wrap">
+    <div id="content" role="main">
+
+      <!-- Action hook to insert content before the loop starts -->
+      <?php agriflex_before_loop(); ?>
+
+      <?php if ( have_posts() ) : ?>
+
+        <?php agriflex_content_nav( 'nav-above' ); ?>
+
+        <?php while ( have_posts() ) : the_post(); ?>
+
+          <!-- Get the category or post format the post belongs to -->
+          <?php $format = agriflex_get_format(); ?>
+
+          <!-- Run the loop to output the posts.
+          If you want to overload this in a child theme then include a file
+          called content-index.php and that will be used instead. -->
+          <?php get_template_part( 'content-college-news', $format ); ?>
+
+        <?php endwhile; ?>
+
+        <?php agriflex_content_nav( 'nav-below' ); ?>
+
+      <?php else : ?>
+
+        <?php get_template_part( 'no-results', 'index' ); ?>
+
+      <?php endif; ?>
+
+      <!-- Action hook to insert content after the loop -->
+      <?php agriflex_after_loop(); ?>
+
+    </div><!-- #content -->
+  </div><!-- #container -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
