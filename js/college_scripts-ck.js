c7feeeb63d294d8b603352f90e0c199c37f7cd3b
Travis Ward <travis@travsiward.com>
2013-02-14 14:13:56 -0600

    Add Styles for Special Recent Posts plugin (SRP)

--- js/college_scripts-ck.js
+++ js/college_scripts-ck.js
@@ -1,3 +1,36 @@
+/*
+*	This handles the body classes for off-canvas actions
+*	Regex Help Here: http://ext.ag/WeinOg
+*/
+
+var changeActive = function() {
+	var page = document.getElementById("page-body");
+
+	if ( page.className.match(/(?:^|\s)not-active(?!\S)/) ) {
+		page.className = page.className.replace( /(?:^|\s)not-active(?!\S)/g , ' active-sidebar' );
+	} else if ( page.className.match(/(?:^|\s)active-sidebar(?!\S)/) ) {
+		page.className = page.className.replace( /(?:^|\s)active-sidebar(?!\S)/g , ' not-active' );
+	}
+};
+
+window.onload = function() {
+	var sidebar_button = document.getElementById("menu-button");
+	sidebar_button.onclick = function(event) {
+		changeActive();
+		event.preventDefault();
+	};
+
+};
+
+window.onresize = function() {
+	var page = document.getElementById("page-body");
+	page.className = page.className.replace( /(?:^|\s)active-sidebar(?!\S)/g , ' not-active' );
+};
+
+/* **********************************************
+     Begin college_scripts.js
+********************************************** */
+
 jQuery(document).ready(function($) {
 
     //set the initial values
@@ -177,39 +210,6 @@ jQuery(document).ready(function($) {
 	$('.button').wrap('<div class="button-wrap" />');
 
 });
-
-/* **********************************************
-     Begin off-canvas.js
-********************************************** */
-
-/*
-*	This handles the body classes for off-canvas actions
-*	Regex Help Here: http://ext.ag/WeinOg
-*/
-
-var changeActive = function() {
-	var page = document.getElementById("page-body");
-
-	if ( page.className.match(/(?:^|\s)not-active(?!\S)/) ) {
-		page.className = page.className.replace( /(?:^|\s)not-active(?!\S)/g , ' active-sidebar' );
-	} else if ( page.className.match(/(?:^|\s)active-sidebar(?!\S)/) ) {
-		page.className = page.className.replace( /(?:^|\s)active-sidebar(?!\S)/g , ' not-active' );
-	}
-};
-
-window.onload = function() {
-	var sidebar_button = document.getElementById("menu-button");
-	sidebar_button.onclick = function(event) {
-		changeActive();
-		event.preventDefault();
-	};
-
-};
-
-window.onresize = function() {
-	var page = document.getElementById("page-body");
-	page.className = page.className.replace( /(?:^|\s)active-sidebar(?!\S)/g , ' not-active' );
-};
 
 /* **********************************************
      Begin superfish-menu.js
