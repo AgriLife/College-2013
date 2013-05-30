24fac5cdc1388833acb235d53495036b940fcdc1
Travis Ward <travis@travsiward.com>
2013-05-30 17:10:04 -0500

    Drop Modernizr

--- functions.php
+++ functions.php
@@ -75,6 +75,9 @@ function load_new_js() {
   // drop fitvids
   wp_deregister_script( 'fitvids' );
 
+  // drop Modernizr
+  wp_deregister_script( 'modernizr' );
+
 	if ( !is_admin() ) {
 		wp_deregister_script( 'my_scripts' );
 
