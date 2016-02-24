1c0b230117d1e548b3ca73ee56a5f7440fe06386
Zachary Watkins <watkinza@gmail.com>
2016-02-24 14:29:31 -0600

    Correct college priority links on home page.

--- page-home.php
+++ page-home.php
@@ -15,13 +15,13 @@ get_header(); ?>
 				<ul class="featured-stories">
 					<?php ob_start(); ?>
 					<li class="challenge-hign challenge">						
-						<a href="<?php echo site_url('/academics/high-impact-learning/') ?>" id="l1" class="challenge-link">
+						<a href="http://aglifesciences.tamu.edu/academics/high-impact-learning/" id="l1" class="challenge-link">
 							<h2 id="challenge-high1">High-Impact</h2>
 							<h2 id="challenge-high2">Learning</h2>
 						</a>
 					</li>				
 					<li class="challenge-stem challenge">
-						<a href="<?php echo site_url('/about/stem/') ?>" id="l2" class="challenge-link">
+						<a href="http://aglifesciences.tamu.edu/about/stem/" id="l2" class="challenge-link">
 							<h2 id="challenge-stem1">Science &bull; Technology</h2>
 							<h2 id="challenge-stem2">Engineering &bull; Math</h2>
 						</a>		
@@ -33,17 +33,17 @@ get_header(); ?>
 						</a>		
 					</li>				
 					<li class="challenge-diversity challenge">
-						<a href="<?php echo site_url('/about/diversity/') ?>" id="l4" class="challenge-link">
+						<a href="http://aglifesciences.tamu.edu/about/diversity/" id="l4" class="challenge-link">
 							<h2 id="challenge-diversity">Diversity</h2>
 						</a>		
 					</li>				
 					<li class="challenge-accountability challenge">
-						<a href="<?php echo site_url('/about/accountability/') ?>" id="l5" class="challenge-link">
+						<a href="http://aglifesciences.tamu.edu/about/accountability/" id="l5" class="challenge-link">
 							<h2 id="challenge-accountability">Accountability</h2>
 						</a>		
 					</li>
 					<li class="challenge-international challenge">
-						<a href="<?php echo site_url('/academics/international-programs/') ?>" id="l6" class="challenge-link">
+						<a href="http://aglifesciences.tamu.edu/academics/international-programs/" id="l6" class="challenge-link">
 							<h2 id="challenge-international1">International</h2>
 							<h2 id="challenge-international2">Programs</h2>
 						</a>		
