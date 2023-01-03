8c598f5905ee0f728ceebdbf3aa3f9e07d41fed1
s-schiller <118216848+s-schiller@users.noreply.github.com>
2023-01-03 13:39:36 -0600

    Update functions.php
    Updated links to Statewide Search, Veterans Benefits, and Military Families in footer

--- functions.php
+++ functions.php
@@ -513,8 +513,8 @@ function agriflex_college_footer() {
   		<li><a href="http://agrilife.org/required-links/privacy/">Privacy and Security</a></li>
   		<li><a href="http://itaccessibility.tamu.edu/" target="_blank">Accessibility Policy</a></li>
   		<li><a href="http://publishingext.dir.texas.gov/portal/internal/resources/DocumentLibrary/State%20Website%20Linking%20and%20Privacy%20Policy.pdf" target="_blank">State Link Policy</a></li>
-  		<li><a href="http://www.tsl.state.tx.us/trail" target="_blank">Statewide Search</a></li>
-  		<li><a href="http://www.tamus.edu/veterans/" target="_blank">Veterans Benefits</a></li>
+  		<li><a href="https://www.tsl.texas.gov/trail/index.html" target="_blank">Statewide Search</a></li>
+  		<li><a href="https://aggie.tamu.edu/financial-aid/veterans" target="_blank">Veterans Benefits</a></li>
   		<li><a href="https://fch.tamu.edu/programs/military-programs/" target="_blank">Military Families</a></li>
   		<li><a href="https://secure.ethicspoint.com/domain/en/report_custom.asp?clientid=19681" target="_blank">Risk, Fraud &amp; Misconduct Hotline</a></li>
   		<li><a href="http://governor.state.tx.us/homeland/" target="_blank">Texas Homeland Security</a></li>
